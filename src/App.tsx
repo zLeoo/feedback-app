9d5c06ab6f3eab61922b54f1a32334d2d7dbbdd8
Leonardo <leozikasantos73@gmail.com>
2021-10-07 01:33:50 -0300

    chore: setup styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import styled from "styled-components";
+
+const HelloWorld = styled.h1`
+  color: chocolate;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <HelloWorld>Teste</HelloWorld>;
 }
 
 export default App;
